bdb98a54d6159582f86ac6328f36c4d5d97c0626
yo-shi-mi <erin001tw@gmail.com>
2021-02-28 02:36:51 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+  <ul>
+    <li><a href="#area1">Shadowhunters</a></li>
+    <li><a href="#area2">Reign</a></li>
+    <li><a href="#area3">13 reasons why</a></li>
+    <li><a href="#area4">Riverdale</a></li>
+    <li><a href="#area5">Insatiable</a></li>
+  </ul>
+</div>
+<div className="area1" id="area1">
+  <div className="call">
+    Introduction: Clary Fray has just enrolled at Brooklyn Academy of Arts. On her eighteenth birthday, she learns that she is a Shadowhunter, a human born with angelic blood who protects humans from demons. That night, Clary's mother, Jocelyn, is abducted by a group of rogue Shadowhunters called the Circle. Their leader is Jocelyn's ex-husband, Valentine Morgenstern. 
+With her mother missing, Clary turns to Luke, a person she trusted, only to seemingly be betrayed. Clary joins up with a band of Shadowhunters to save her mother and discovers powers she never knew she possessed. Clary is thrown into the world of demon hunting with the mysterious, narcissistic, and attractive Shadowhunter Jace, and also her loyal and geeky friend Simon. Now living amongst seelies, warlocks, vampires, and werewolves, Clary begins a journey of self-discovery as she learns more about her past and what her future may hold.
+  </div>
+</div>
+<div className="area2" id="area2">
+  <div className="call2">
+    Introduction: The highly fictionalized series follows the early exploits of Mary, Queen of Scots. The first season opens in 1557, with Mary living in a convent in France, but she quickly is returned to the castle where we learn she is awaiting her marriage to Prince Francis, to whom she has been engaged since they were six. Mary has to contend with changing politics and power plays, as well as her burgeoning feelings for Francis and the romantic attentions of Francis's bastard half-brother, Bash. Francis's mother, Catherine de' Medici, secretly tries to prevent the marriage following Nostradamus's confidential prediction that the marriage will lead to Francis's death. The series also follows the affairs of Mary's Scottish handmaidens Kenna, Aylee, Lola, and Greer, who are searching for husbands of their own at French Court. 
+  </div>
+</div>
+<div className="area3"id="area3">
+  <div className="call3">
+    Introduction:In season one, seventeen year old Clay Jensen returns home from school one day to find a mysterious box on his porch. Inside he discovers seven cassette tapes recorded by Hannah Baker, his deceased classNamemate and unrequited love, who killed herself two weeks earlier. On the tapes, Hannah unfolds an intensely emotional audio diary, detailing why she decided to end her life. It appears each person who receives this package of old-style tapes is fundamentally related to why she killed herself. Clay is not the first to receive the tapes, but there is implied detail as to how he should pass the tapes on after hearing them. There appears to be an order to distribution of the tapes, with an additional copy held by an overseer should the plan go awry. Each tape recording refers to a different person involved in Hannah's life contributing to a reason for her suicide. The tapes refer to both friends and enemies. 
+  </div>
+</div>
+<div className="area4"id="area4">
+  <div className="call4">
+    Introduction:While navigating the troubled waters of sex, romance, school and family, teen Archie and his gang become entangled in a dark Riverdale mystery.
+  </div>
+</div>
+<div className="area5"id="area5">
+  <div className="call5">
+   Introduction: Patty Bladell is a teenager who was constantly bullied in school for being overweight. After being on a liquid diet for three months over summer vacation, she is now thin and seeks revenge on her bullies. A disgraced civil lawyer and obsessed beauty pageant coach, Bob Armstrong, notices Patty's potential and sets out to turn her into a pageant queen.
+  </div>
+</div>
     </div>
   );
 }
